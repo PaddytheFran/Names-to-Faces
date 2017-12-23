f79b72b2b989af71c8e24120e342d6eb5fa68ddb
Patrick Flanagan <patrickflanagan@patricks-imac.local>
2017-12-23 15:43:40 -0600

    Data sources update, process to import photos started.

--- HackingWithSwift - Project10/ViewController.swift
+++ HackingWithSwift - Project10/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
-
+class ViewController: UICollectionViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+		navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addNewPerson))
+	
+	}
+
+	@objc func addNewPerson() {
+		
+		let picker = UIImagePickerController()
+		picker.allowsEditing = true
+		picker.delegate = self
+		present(picker, animated: true)
+		
+	}
+	
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+	override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+		return 10
+
+	}
+	
+	override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+		
+		let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as! PersonCell
+		return cell
+		
+	}
+	
 
 }
 
