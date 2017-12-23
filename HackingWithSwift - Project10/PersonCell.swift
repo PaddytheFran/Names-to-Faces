b21733a060cd936c41e9ec3c4a2c90e21656f520
Patrick Flanagan <patrickflanagan@patricks-imac.local>
2017-12-23 10:05:14 -0600

    Image and Label connections created to View Controller.

---
+++ HackingWithSwift - Project10/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  HackingWithSwift - Project10
+//
+//  Created by Patrick Flanagan on 12/23/17.
+//  Copyright © 2017 Patrick Flanagan. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+	
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var name: UILabel!
+	
+}
